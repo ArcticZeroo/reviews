2c9ea17f9dd9b277b01ec999a20939fec89423d6
Spencer <spencer@frozor.io>
2021-10-14 23:47:13 -0700

    Add saving visit

--- client/src/components/pages/log/visit-log.tsx
+++ client/src/components/pages/log/visit-log.tsx
@@ -1,21 +1,26 @@
 import React, { useState } from 'react';
 import { SearchBar } from '../../search/search-bar';
-import { IPointOfInterest } from '../../../models/location';
+import { IPointOfInterest, LocationNameOrId } from '../../../models/location';
 import { VisitReview } from './visit-review';
 import { VisitSentimentList } from './visit-sentiment-list';
-import { ISentimentEntity, ISentimentEntityWithId, ITextAnalysisResult } from '../../../models/analysis';
+import { ISentimentEntityWithId, ITextAnalysisResult } from '../../../models/analysis';
 import { spaceBottomOfChildren } from '../../../constants/css';
 import * as random from '../../../util/random';
 import styled from 'styled-components';
 import { LoadingOverlay } from '../../overlay/loading/loading-overlay';
 import { SentimentAnalysisClient } from '../../../api/analysis/client';
 import { positiveScore, sentimentToScore } from '../../../util/sentiment';
+import { PrimaryButton } from '../../styled/button';
+import { VisitStorage } from '../../../api/storage/idb/visit';
+import { ISerializedVisit } from '../../../models/visit';
+import { useHistory } from 'react-router-dom';
 
 const VisitReviewContainer = styled.div`
   position: relative;
 `;
 
 const VisitLogContainer = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   ${spaceBottomOfChildren};
@@ -33,14 +38,17 @@ const getDefaultSentimentEntity = () => ({
 });
 
 const sentimentAnalysisClient = new SentimentAnalysisClient();
+const visitStorageClient = new VisitStorage();
 
 export const VisitLog: React.FC = () => {
+    const history = useHistory();
     const [locationQuery, setLocationQuery] = useState('');
     const [enhancedLocation, setEnhancedLocation] = useState<IPointOfInterest | undefined>();
     const [reviewText, setReviewText] = useState<string>('');
     const [sentimentEntities, setSentimentEntities] = useState<Array<ISentimentEntityWithId>>(() => [getDefaultSentimentEntity()]);
     const [isGeneratingSentiment, setIsGeneratingSentiment] = useState(false);
     const [sentimentGenerationError, setSentimentGenerationError] = useState<any>();
+    const [isSavingVisit, setSavingVisit] = useState(false);
 
     const onLocationQueryChanged = (newQuery: string) => {
         setLocationQuery(newQuery);
@@ -106,8 +114,43 @@ export const VisitLog: React.FC = () => {
             });
     };
 
+    const onSaveClicked = () => {
+        setSavingVisit(true);
+
+        const serializedLocation: LocationNameOrId = enhancedLocation ? {
+            type: 'id',
+            data: enhancedLocation.id
+        } : {
+            type: 'name',
+            data: locationQuery
+        };
+
+        visitStorageClient.store({
+            location: serializedLocation,
+            visitedAt: new Date(),
+            review: reviewText,
+            sentiment: {
+                overallSentiment: {
+                    positive: 1.0,
+                    negative: 0
+                },
+                sentimentEntities
+            }
+        })
+            .finally(() => setSavingVisit(false))
+            .then(() => {
+                history.push('/');
+            })
+            .catch(console.error);
+    };
+
     return (
         <VisitLogContainer>
+            {
+                isSavingVisit && (
+                    <LoadingOverlay/>
+                )
+            }
             <SearchBar immediateQuery={locationQuery}
                        selectedEnhancedLocation={enhancedLocation}
                        onImmediateQueryChanged={onLocationQueryChanged}
@@ -122,6 +165,9 @@ export const VisitLog: React.FC = () => {
                                 onSentimentEntityChanged={onSentimentEntityChanged}
                                 onSentimentEntityAdded={onSentimentEntityAdded}
                                 onSentimentEntityRemoved={onSentimentEntityRemoved}/>
+            <PrimaryButton onClick={onSaveClicked} disabled={!locationQuery || sentimentEntities.length === 0}>
+                Save Visit
+            </PrimaryButton>
         </VisitLogContainer>
     );
 };
--- client/src/components/styled/button.tsx
+++ client/src/components/styled/button.tsx
@@ -7,6 +7,11 @@ export const PrimaryButton = styled.button`
   border: none;
   border-radius: 0.5rem;
   padding: 0.5rem 1rem 0.5rem 1rem;
+  
+  &:disabled {
+    filter: grayscale(25%);
+    opacity: 0.75;
+  }
 `;
 
 export const DestructiveButton = styled(PrimaryButton)`
